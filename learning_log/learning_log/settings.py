735221995ad90cce4b366dd8b6d193c98c75e4a2
Alex <tomattman@gmail.com>
2017-10-12 23:24:56 +0300

    add users support

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [	
+INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     #My apps
     'learning_logs',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = '/users/login/'
--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Topic(models.Model):
     """User's topic"""
     text = models.CharField(max_length = 200)
     date_added = models.DateTimeField(auto_now_add = True)
+    owner = models.ForeignKey(User)
 
     def __str__(self):
         return self.text
